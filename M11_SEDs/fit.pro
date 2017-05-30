db5b1a1c3c0e2e981184844d3466bc35157a180d
James Mullaney <Home@dyn007083.shef.ac.uk>
2017-05-30 18:09:57 +0100

    Now fitting using lmfit.

--- M11_SEDs/fit.pro
+++ M11_SEDs/fit.pro
@@ -8,33 +8,33 @@ PRO fit
   
   files = file_search('SB*')
   for i=0, n_elements(files)-1 DO BEGIN
-  readcol, files[i], wav, flux
-  o = WHERE(wav GE 6. AND wav LT 35.)
-  fit=pahfit(wav[o], flux[o], flux[o]/10)
+     readcol, files[i], wav, flux
+     o = WHERE(wav GE 6. AND wav LT 35.)
+     fit=pahfit(wav[o], flux[o], flux[o]/10)
 
-  mn=1.
-  mx=25.
-  lam=mn+findgen(2400)/(2400)*(mx-mn)
+     mn=1.
+     mx=25.
+     lam=mn+findgen(2400)/(2400)*(mx-mn)
 
-  yfit=pahfit_components(lam,fit,DUST_CONTINUUM=dusts, $
-                         TOTAL_DUST_CONTINUUM=dust_tot,STARLIGHT=stars, $
-                         DUST_FEATURES=features, $
-                         TOTAL_DUST_FEATURES=features_tot, $
-                         LINES=lines,TOTAL_LINES=lines_tot, $
-                         EXTINCTION_FAC=ext,_EXTRA=e)
-  rat = fltarr(n_elements(lam))
-  for j=0,(size(features,/DIMENSIONS))[1]-1 do begin 
-     rat+=(features[*,j])/lam
-  endfor 
+     yfit=pahfit_components(lam,fit,DUST_CONTINUUM=dusts, $
+                            TOTAL_DUST_CONTINUUM=dust_tot,STARLIGHT=stars, $
+                            DUST_FEATURES=features, $
+                            TOTAL_DUST_FEATURES=features_tot, $
+                            LINES=lines,TOTAL_LINES=lines_tot, $
+                            EXTINCTION_FAC=ext,_EXTRA=e)
+     rat = fltarr(n_elements(lam))
+     for j=0,(size(features,/DIMENSIONS))[1]-1 do begin 
+        rat+=(features[*,j])/lam
+     endfor 
 
-  ;oplot, lam, rat/max(rat)
-  if n_elements(allrat) EQ 0 then allrat = fltarr(n_elements(lam), 5)
-  allrat[*,i] = rat/max(rat)
+  oplot, lam, rat/max(rat)
+     if n_elements(allrat) EQ 0 then allrat = fltarr(n_elements(lam), 5)
+     allrat[*,i] = rat/max(rat)
   
-endfor
+  endfor
 
   resistant_mean, allrat, 2., mean, dim=2
-  oplot, lam, mean;, color=fsc_color('Red')
+  oplot, lam, mean              ;, color=fsc_color('Red')
 
   forprint, lam, mean, textout='avg_pah.dat'
   
---
+++ sedfit.py
@@ -0,0 +1,166 @@
+import numpy as np
+#from astropy import units as u
+#%matplotlib inline
+import matplotlib.pyplot as plt
+#from astropy.analytic_functions import \
+#                blackbody_nu as bb_nu
+from astropy import constants as const
+from lmfit import minimize, Parameters
+
+class galsed():
+    
+    #Speed of light in um/s:
+    c = 1e6*(const.c).value
+    h = (const.h).value
+    kb = (const.k_B).value
+    
+    def __init__(self, \
+                 norm=1., temp=25., beta=1.5, \
+                 plnorm=0.5, alpha=2., turnover=50., \
+                 pah=0.1):
+        
+        #Set the wavelength array; can be changed with setwav():    
+        self.wav = np.logspace(np.log10(5),np.log10(1000),1024)
+        self.nu = self.c/self.wav
+        
+        #Set default properties; can be changed with setbb():    
+        self.params = {'bb_norm':norm,\
+                       'bb_temp':temp,\
+                       'beta':beta,\
+                       'plnorm':plnorm,\
+                       'alpha':alpha,\
+                       'tp':turnover,\
+                       'pahnorm':pah}
+
+        #Read in the PAH features:
+        self.pwav, self.pflux = np.loadtxt('M11_SEDs/avg_pah.dat', \
+                                 comments='#', \
+                                 unpack=True)  
+        self.pah = np.interp(self.wav,self.pwav,self.pflux,\
+                             left=0.,right=0.)
+                                                        
+    def setwav(self, wav):
+        #(Re)Define the wavelength array (in microns):    
+        self.wav = wav
+        self.nu = self.c/self.wav
+        self.pah = np.interp(self.wav,self.pwav,self.pflux,\
+                             left=0.,right=0.)    
+        
+    def setbb(self, **kwargs):
+        #(Re)Define the Blackbody parameters:
+        #kwargs is used to maintain current values by default
+        if 'bbnorm' in kwargs: 
+            self.params['bb_norm'] = kwargs.get('bbnorm')
+        if 'temp' in kwargs: 
+            self.params['bb_temp'] = kwargs.get('temp')
+        if 'beta' in kwargs: 
+            self.params['beta'] = kwargs.get('beta')
+
+    def setpl(self, **kwargs):
+        #(Re)Define the Blackbody parameters:
+        if 'plnorm' in kwargs:
+            self.params['plnorm'] = kwargs.get('plnorm')
+        if 'alpha' in kwargs:
+            self.params['alpha'] = kwargs.get('alpha')
+        if 'turnover' in kwargs:
+            self.params['tp'] = kwargs.get('turnover')
+        
+    def setpah(self, **kwargs):
+        if 'pahnorm' in kwargs:
+            self.params['pahnorm'] = kwargs.get('pahnorm')
+    
+    def setsed(self, **kwargs):
+        #This is just a shorthand for setting the three SED components:    
+        self.setpl(**kwargs)    
+        self.setbb(**kwargs)
+        self.setpah(**kwargs)
+        
+    def getwav(self):
+        return self.wav
+    
+    def bb(self, wav, temp):
+        
+        #Use own BB generator (ripped from astropy);
+        #astropy's is slow, largely due to checks.
+        nu = self.c/wav
+        log_boltz = self.h * nu / (self.kb * temp)
+        boltzm1 = np.expm1(log_boltz)
+        return (2.0 * self.h * nu ** 3 / \
+                (self.c ** 2 * boltzm1))
+    
+    def getbb(self):
+        
+        #Get the modified BB spectrum:    
+        #Calculate normalisation:
+        wavpeak = 2.9e3/self.params['bb_temp']
+        nupeak = self.c/wavpeak
+        norm = self.params['bb_norm']/((nupeak**self.params['beta'])*\
+                       self.bb(wavpeak, self.params['bb_temp']))
+        
+        bb = self.bb(self.wav, self.params['bb_temp'])
+        return norm*bb*(self.nu**self.params['beta'])
+        
+    def getpl(self):
+        #Get PL spectrum:
+        norm = self.params['bb_norm']*self.params['plnorm']/\
+            ((self.params['tp']**self.params['alpha'])*np.exp(-1.))
+        
+        return norm*(self.wav**self.params['alpha'])*\
+                             np.exp(-(self.wav/self.params['tp'])**2.)
+    
+    def getpah(self):
+        return self.params['pahnorm']*self.pah
+    
+    def getsed(self):
+        return self.getpl()+self.getbb()+self.getpah()
+    
+    def fitfunc(self, pars, x, y, err):
+
+        for key, value in self.params.iteritems():
+            self.params[key] = pars[key].value
+            
+        return (self.getsed() - y)/err
+
+    def fit(self, y, err):
+        
+        pars = Parameters()
+        for key, value in self.params.iteritems():
+            pars.add(key, value=value)
+
+        #pars['alpha'].set(vary=False)
+        #pars['plnorm'].set(vary=False)
+        pars['tp'].set(max=70.)
+        
+        return minimize(self.fitfunc, pars, args=(self.wav, y, err))
+        
+wav, flux = np.loadtxt('M11_SEDs/SB1.dat', \
+                                 comments='#', \
+                                 unpack=True)
+o = np.where(wav < 500.)
+wav = wav[o]
+flux = flux[o]
+
+flux = flux/np.max(flux)
+sed = galsed()
+sed.setwav(wav)
+sed.setpl(alpha=2.8,plnorm=0.2,turnover=45)
+
+err = flux/5.
+o = np.where(wav < 35.)
+err[o] = 1.0*err[o]
+
+result = sed.fit(flux, err)
+
+result.params.pretty_print()
+print result.redchi
+
+plt.plot(wav, flux, \
+         #wav, sed.getpl(), \
+         #wav, sed.getbb(), \
+         #wav, sed.getpah(), \
+         wav, sed.getsed())
+plt.axis([6,600,0.001,2])
+plt.xscale('log')
+plt.yscale('log')
+plt.show()
+
