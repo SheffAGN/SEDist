1f8ec1c1ae2f74578fe8df516fd81c5c1e015ef8
James Mullaney <Home@dyn006131.shef.ac.uk>
2017-06-16 11:48:57 +0100

    Added more filters

--- test.py
+++ test.py
@@ -1,8 +1,15 @@
 import numpy as np
+from pymc3 import Model, Normal, DensityDist
+from pymc3 import NUTS, sample
 from sedfit import source, photset
 
-pho = photset()
+#Create a source:
+src = source(0,0,0.1)
+src.sed.setBB(temp=40.)
+src.sed.setPL(alpha=2., turnover=50.)
 
+#Generate a filter set and add filters:
+pho = photset()
 wav,trans = np.loadtxt('./sedfit/Filters/PACS/PacsFilter_red.txt', \
                        unpack=True)
 pho.addFilter('P160',wav,trans)
@@ -13,12 +20,40 @@ wav,trans = np.loadtxt('./sedfit/Filters/PACS/PacsFilter_green.txt', \
                        unpack=True)
 pho.addFilter('P100',wav,trans)
 
-src = source(0,0,0.1)
-flux = pho.getFlux(src)
+sigma = 20.
+obsflux = pho.getFlux(src)
+Y = obsflux.values()+np.random.randn(3)*sigma
 
-ind = np.argsort(flux.keys())
-print np.array(flux.keys())[ind]
-print np.array(flux.values())[ind]
-print 'trivial'
-from sedfit import fitter
-fitter.fit1d(0.,src,pho)
+sedmodel = Model()
+with sedmodel:
+    temp = Normal('temp', mu=30, sd=10)
+    alpha = Normal('alpha', mu=2., sd=0.5)
+    turn = Normal('turn', mu=50, sd=10)
+    src.sed.setBB(temp=temp)
+    src.sed.setPL(alpha=alpha, turnover=turn)
+    modflux = (pho.getFlux(src)).values()
+
+    def logp(obs):
+        return -0.5*((modflux-obs)/sigma)**2.
+
+    Y_obs = DensityDist('Y_obs', logp, observed=Y)
+
+    trace = sample(5500)
+quit()
+
+from pymc3 import find_MAP
+from scipy import optimize
+map_estimate = find_MAP(model=sedmodel, fmin=optimize.fmin_powell)
+print(map_estimate)
+
+from pymc3 import NUTS, sample
+from scipy import optimize
+with sedmodel:
+    # draw 500 posterior samples
+    trace = sample(5500)
+print trace['temp']
+
+import matplotlib.pyplot as plt
+from pymc3 import traceplot, summary
+traceplot(trace)
+summary(trace)
