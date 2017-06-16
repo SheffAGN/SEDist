1463249a5c598280e3cc501f4cfad934cd3c8015
James Mullaney <Home@dyn006131.shef.ac.uk>
2017-06-16 15:14:30 +0100

    Minor changes to test model. Still workingpython test.py

--- test.py
+++ test.py
@@ -1,19 +1,12 @@
 import numpy as np
 from pymc3 import Model, Normal, DensityDist
-<<<<<<< HEAD
 from pymc3 import NUTS, sample
-=======
->>>>>>> Theanorise
 from sedfit import source, photset
 
 #Create a source:
 src = source(0,0,0.1)
 src.sed.setBB(temp=40.)
-<<<<<<< HEAD
-src.sed.setPL(alpha=2., turnover=50.)
-=======
 src.sed.setPL(alpha=2.)
->>>>>>> Theanorise
 
 #Generate a filter set and add filters:
 pho = photset()
@@ -33,59 +26,23 @@ wav,trans = np.loadtxt('./sedfit/Filters/WISE/WiseFilter_W3.txt', \
                        unpack=True)
 pho.addFilter('W012',wav,trans)
 
-<<<<<<< HEAD
-sigma = 20.
 obsflux = pho.getFlux(src)
-Y = obsflux.values()+np.random.randn(3)*sigma
-=======
-obsflux = pho.getFlux(src)
-sigma = 0.4*obsflux.tag.test_value
+sigma = 0.1*obsflux.tag.test_value
 Y = obsflux+np.random.randn(5)*sigma
-print Y.tag.test_value
->>>>>>> Theanorise
 
 sedmodel = Model()
 with sedmodel:
-    temp = Normal('temp', mu=30, sd=10)
+    temp = Normal('temp', mu=40, sd=10)
     alpha = Normal('alpha', mu=2., sd=0.5)
-<<<<<<< HEAD
-    turn = Normal('turn', mu=50, sd=10)
-    src.sed.setBB(temp=temp)
-    src.sed.setPL(alpha=alpha, turnover=turn)
-    modflux = (pho.getFlux(src)).values()
-=======
     src.sed.setBB(temp=temp)
     src.sed.setPL(alpha=alpha)
     modflux = pho.getFlux(src)
-    print modflux.tag
->>>>>>> Theanorise
 
     def logp(obs):
         return -0.5*((modflux-obs)/sigma)**2.
 
     Y_obs = DensityDist('Y_obs', logp, observed=Y)
 
-<<<<<<< HEAD
-    trace = sample(5500)
-quit()
-
-from pymc3 import find_MAP
-from scipy import optimize
-map_estimate = find_MAP(model=sedmodel, fmin=optimize.fmin_powell)
-print(map_estimate)
-
-from pymc3 import NUTS, sample
-from scipy import optimize
-with sedmodel:
-    # draw 500 posterior samples
-    trace = sample(5500)
-print trace['temp']
-
-import matplotlib.pyplot as plt
-from pymc3 import traceplot, summary
-traceplot(trace)
-summary(trace)
-=======
 #from pymc3 import find_MAP
 #from scipy import optimize
 #map_estimate = find_MAP(model=sedmodel, fmin=optimize.fmin_powell)
@@ -95,6 +52,5 @@ from pymc3 import NUTS, sample, df_summary
 #from scipy import optimize
 with sedmodel:
     # draw 500 posterior samples
-    trace = sample(5500, njobs=4)
+    trace = sample(5500)
 print df_summary(trace)
->>>>>>> Theanorise
